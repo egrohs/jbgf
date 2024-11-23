8a56c10407967e787671faeabe45a133d343c4b7
Emanuel Motta Grohs <emanuel.grohs@serpro.gov.br>
2024-11-23 16:27:50 -0300

    nova versao libgdx

--- core/src/com/mygdx/game/visao/ILayoutStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.mygdx.game.visao;
-
-import com.mygdx.game.model.IPlayer;
-
-public interface ILayoutStrategy {
-	public void reorganiza(IPlayer modelo);
-}
---
+++ core/src/main/java/com/mygdx/game/tolibgdx/ComponenteAdapterActor.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.tolibgdx;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class ComponenteAdapterActor {
+private Actor gdxActor;
+}
